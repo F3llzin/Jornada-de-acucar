4f62ac26a61a58f72f5d41f2bbe910804dc1ee4c
F3llzin <fellipefernandes784@gmail.com>
2024-08-25 23:08:23 -0300

    Criação do personagem principal e inicio do mundo um.

--- scripts/Telainicio.gd
+++ scripts/Telainicio.gd
@@ -1,14 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
 
 
 func _on_button_start_pressed():
@@ -17,3 +9,4 @@ func _on_button_start_pressed():
 
 func _on_button_quit_pressed():
 	get_tree().quit()
+
