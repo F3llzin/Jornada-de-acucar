0736387e8e2d7f845293a35142de25b6444578e5
F3llzin <fellipefernandes784@gmail.com>
2024-08-26 09:40:17 -0300

    Animacao personagem
    animação do personagem do jogo.

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -3,10 +3,11 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -280.0
+@onready var animation = $anim as AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var pulando = false
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -16,13 +17,22 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		pulando = true
+	elif is_on_floor():
+		pulando = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
+		if !pulando:
+			animation.play("run")
+	elif pulando:
+		animation.play("jump")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
