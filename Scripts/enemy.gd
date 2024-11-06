0dee2c624a017803e43e86f55d07e29e10e84fd3
F3llzin <fellipefernandes784@gmail.com>
2024-11-06 12:16:59 -0300

    Commit Enemy
    Movimentação do inimigo, falta algumas funcionalidades.

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,9 +1,11 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 700.0
 const JUMP_VELOCITY = -400.0
-
+var direction = -1
+@onready var Detector_parede = $Detector_parede as RayCast2D
+@onready var anim = $anim as Sprite2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -13,16 +15,15 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	if Detector_parede.is_colliding():
+		direction = direction * -1
+		Detector_parede.scale.x *= -1
+	
+	if direction == 1:
+		anim.flip_h = false
+	else: 
+		anim.flip_h = true
+		
+	velocity.x = direction * SPEED * delta
+	
 	move_and_slide()
