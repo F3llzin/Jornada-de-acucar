8d654a7ccd87898445afa1df25d6933ab86df4d2
F3LLZINx <fellipefernandes784@gmail.com>
2024-12-09 12:34:59 -0300

    .

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -3,9 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -280.0
+var vida = 10
+var knockback = Vector2.ZERO
 @onready var animation = $anim as AnimatedSprite2D
 @onready var pular = $pular as AudioStreamPlayer
 @onready var remote_transform = $RemoteTransform2D
+@onready var ray_direita = $Ray_direita
+@onready var ray_esquerda = $Ray_esquerda
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -38,13 +42,32 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animation.play("idle")
+		
+	if knockback != Vector2.ZERO:
+		velocity = knockback
 
 	move_and_slide()
 
 
 func _on_hurt_box_body_entered(body):
-	if body.is_in_group("inimigos"):
-		queue_free()
+	#if body.is_in_group("inimigos"):
+		#queue_free()
+		if vida < 0:
+			queue_free()
+		else:
+			if ray_direita.is_colliding():
+				tomar_dano(Vector2(-200, -200))
+			elif ray_esquerda.is_colliding():
+				tomar_dano(Vector2(200, -200))
+		
 func seguir_camera(camera_2d):
 	var caminho_camera = camera_2d.get_path()
 	remote_transform.remote_path = caminho_camera
+	
+func tomar_dano(knockback_force = Vector2.ZERO, duration = 0.25):
+	vida -= 1
+	if knockback_force != Vector2.ZERO:
+		knockback = knockback_force 
+		
+		var knockback_tween = get_tree().create_tween()
+		knockback_tween.tween_property(self, "knockback", Vector2.ZERO, duration)
