f42a0caa06afbdce35263061b28646c86059e079
F3llzin <fellipefernandes784@gmail.com>
2025-02-06 22:42:39 -0300

    ADD de particula tela splash.
    ADD de particula tela splash.

---
+++ Scripts/particula.gd
@@ -0,0 +1,18 @@
+extends Node2D
+@export var object_templates: Array[PackedScene]
+
+
+func _input(event: InputEvent) -> void:
+	# Check if event is a left click
+	if event is InputEventMouseButton:
+		if event.button_index == 1:
+			if event.pressed:
+				spawn_object(event.position)
+
+
+func spawn_object(position: Vector2) -> void:
+	var index: int = randi_range(0, object_templates.size() - 1)
+	var object_template = object_templates[index]
+	var object = object_template.instantiate()
+	object.position = position
+	add_child(object)
